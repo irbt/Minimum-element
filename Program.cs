f22ca3f80c3a6a0c72599b8d2d43b98be13af4d8
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-12-01 14:09:31 +0100

    MInimum element

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Given an array of integers and array's length, write a method that returns its minimum element.
+
+
+namespace Program{
+    class IsPalindromeRecursionTask{
+            static int MinimumElement( int [] arr, int size ){
+               if (size == 1 ){
+
+               return arr[0];
+
+               }
+
+                return  arr[size-1]< MinimumElement(arr , size -1)? arr[size-1] : MinimumElement( arr , size -1);
+                
+               
+                  
+                    
+            }
+        static void Main(string [] args){
+            
+            Console.WriteLine(MinimumElement(new int[] { 1, 2, 9, 5, 4 }, 5));                  // 2
+            Console.WriteLine(MinimumElement(new int[] { -145, -6, 39, 96, -20, 0, -100 }, 7));  // -100
+            Console.WriteLine(MinimumElement(new int[] { 820, 905, 999, 831, 920, 900 }, 6));   // 830
+        }
+    }
+}
